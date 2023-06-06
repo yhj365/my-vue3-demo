3ff20b7a05bf253770bf5fa4ab124e0bb9f14ad4
yehj <365435306@qq.com>
2023-06-06 18:31:57 +0800

    patch方法实现

--- src/runtime/render.js
+++ src/runtime/render.js
@@ -6,6 +6,9 @@ const SHAPEFLAG = {
   TEXT_CHILDREN: 1 << 3,
   ARRAY_CHILDREN: 1 << 4
 }
+const Text = Symbol("Text")
+//js当中 事件都是以on开头
+const eventReg = /^on[A-Z]/
 
 // 创建一个vnode
 const h = (type, props, children)=>{
@@ -40,8 +43,151 @@ const h = (type, props, children)=>{
 }
 
 export const render = (vnode, container)=> {
-  container.innerHTML = ''
+  const prevNode = container._vnode
+  if(vnode){
+    patch(prevNode, vnode, container)
+  }else{
+    prevNode && unmount(prevNode)
+  }
+  // container.innerHTML = ''
   mount(vnode, container)
+}
+
+export const processTextNode = (n1, n2, container)=>{
+  if(n1){
+    patchTextNode(n1, n2)
+  }else{
+    mountTextNode(n2,container)
+  }
+}
+export const processElement = (n1, n2, container)=>{
+  if(n1){
+    patchElement(n1, n2)
+  }else{
+    mountElement(n2,container)
+  }
+}
+export const patch = (n1, n2, container)=>{
+  if(n1 && n1.type !== n2.type){
+    unmount(n1);
+    nl = null;
+  }
+  const { shapeFlag } = n2
+  if(shapeFlag & SHAPEFLAG.TEXT){
+    processTextNode(n1,n2,container)
+  }else if(shapeFlag & SHAPEFLAG.ELEMENT){
+    processElement(n1,n2,container)
+  }
+}
+
+const patchTextNode = (n1, n2) =>{
+  n2.el = n1.el
+  n1.el.textContent = n2.children
+}
+
+const patchElement = (n1, n2) =>{
+  n2.el = n1.el
+
+  patchProps(n1.props, n2.props, n2.el)
+  patchChildren(n1, n2, n2.el)
+}
+
+const patchProps = (oldProps, newProps, el)=>{
+  if (oldprops === newProps) {
+    return
+  }
+  for (const key in oldprops) {
+    if (newprops[key] == null) {
+      patchDomProps(oldprops[key] , null , key , el)
+    }
+  }
+  for (const key in newProps) {
+    const prev = oldprops[keyl]
+    const next = newProps[key]
+    if(prev !== next){
+      patchDomProps(prev, next , key , el)
+    }
+  }
+}
+
+const patchDomProps = (prev, next , key , el)=>{
+  switch (key) {
+    case 'class':
+      el.className = next || ''
+      break;
+    case 'style':
+      if (next == null) {
+        el.removeAttribute('style');
+      }else {
+        if (prev) {
+          for (const styleName in prev) {
+            if (next[styleName] == null) {
+              el.style[styleName] = ''
+            }
+          }
+        }
+        for (const styleName in next) {
+          el .style[styleName] = next[styleName]
+        }
+      }
+      break;
+    default:
+      if (eventReg.test(key)) {
+        const eventName = key.slice(2).toLowerCase()
+        if (prev) {
+          el.removeEventListener(eventName, prev)
+        }
+        if (next) {
+          el.addEventListener(eventName, next)
+        }
+      }else{
+        if (next == null || next == false) {
+          el.removeEventListener(key)
+        }
+        if (next) {
+          el.addEventListener(key, next)
+        }
+      }
+      break;
+  }
+}
+
+const patchChildren = (n1, n2,container)=>{
+  const {shapeflag: prevShapeFlag, children: prevChildren}  = n1
+  const {shapeflag: nextShapeFlag, children: nextChildren}  = n2
+
+  if (prevShapeFlag & SHAPEFLAG.TEXT_CHILDREN){
+    if (nextShapeFlag & SHAPEFLAG.TEXT_CHILDREN) {
+      container.textContent = nextChildren
+    }else if (nextShapeFlag & SHAPEFLAG.ARRAY_CHILDREN){
+      container.textContent = '';
+      moutChildren(nextChildren, container)}
+    else{
+      container.textContent = ''
+    }
+  }else if(prevShapeFlag & SHAPEFLAG.ARRAY_CHILDREN){
+    if(nextShapeFlag & SHAPEFLAG.TEXT_CHILDREN){
+      unmountChildren(prevChildren)
+      container.textContent = nextChildren
+    }else if(nextShapeFlag & SHAPEFLAG.ARRAY_CHILDREN){
+      patchArrayChildren(prevChildren, nextChildren, container)
+    }else{
+      unmountChildren(nextChildren, container)
+    }
+  }
+}
+const patchArrayChildren = (prev, next,container)=>{
+  const oldLength = prev.length;
+  const newLength = next.length;
+  const commomLengtn = Math.min(oldLength, newLength)
+  for (let i = 0; i < commomLengtn; i++){
+    patch(prev[i],next[i], container)
+  }
+  if(oldLength > newLength){
+    unmountChildren(prev.slice(commomLengtn))
+  }else if(oldlength < newlength){
+    moutChildren(next.slice(commomLengtn),container)
+  }
 }
 
 const mount = (vnode, container)=>{
@@ -86,15 +232,14 @@ const mountElement = (vnode, container) => {
 
 const moutChildren = (children,container) => {
   children.forEach( child => {
-    mount(child ,container)
+    patch(child ,container)
   })
 }
 // 组件
 const mountComponent = (vnode, container) => {
 
 }
-//js当中 事件都是以on开头
-const eventReg = /^on[A-Z]/
+
 
 const mountprops = (props, el) => {
   for(const key in props) {
@@ -123,6 +268,7 @@ const mountprops = (props, el) => {
 
 const unmount = vnode => {
   // 前文说的 el 派上用场了
+  console.log(vnode);
   const {el} = vnode;
   el.parentNode.removeChild(el);
 }
@@ -132,7 +278,7 @@ const unmountChildren = children => {
   });
 }
 
-// 普通渲染
+// 普通渲染test
 // render(
 //   h(
 //     // div ul styoe onClick chilren
@@ -154,3 +300,28 @@ const unmountChildren = children => {
 //   document.getElementById('app')
 // )
 
+// 修改节点重新渲染test
+render(
+  h('ul', null,[
+    h('li', null,'我是 first'),
+    h('li', null,[
+      h('li', null,'我是second 的1'),
+      h('li', null,'我是second 的2'),
+      h('li', null,'我是second 的3'),
+    ]),
+    h('li', null, 'third')
+  ]),
+  document.body
+)
+  setTimeout(() => {
+    h('ul', null,[
+      h('li', null,'first'),
+      h('li', null,[
+        h('li', null,'我是second 的first'),
+        h('li', null,'我是second 的我是second'),
+        h('li', null,'我是second 的third'),
+      ]),
+      h('li', null, 'third')
+    ]),
+  document.body
+  },5000)
